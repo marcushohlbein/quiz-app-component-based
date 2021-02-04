caf4e5555bac2b1847681f2239bd9de36ecf2f5e
Marcus Hohlbein <marcus.hohlbein@gmail.com>
2021-02-04 14:29:33 +0100

    Fixing header component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import Button from './components/Button'
+import Header from './components/Header'
+
+const header = Header('Quiz App', 'may the best win!')
+document.body.append(header)
 
 const button = Button('Click me!')
-
 document.body.append(button)
